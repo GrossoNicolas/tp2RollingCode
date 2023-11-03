8635b88577d8707eb265c2f44e52d7319be4bdec
NicolasGrosso <grossonicolasorl@gmail.com>
2023-11-03 12:42:51 -0300

    Ejercicio numero 5 probado y terminado

---
+++ js/ejercicio5.js
@@ -0,0 +1,26 @@
+
+        while (true) {
+            let numeroDNI = prompt("Por favor, ingrese el número de DNI (0-99999999):");
+
+            
+            if (numeroDNI === null) {
+                alert("Gracias por usar la calculadora de letra del DNI.");
+                break;
+            }
+
+          
+            numeroDNI = parseInt(numeroDNI);
+
+       
+            if (isNaN(numeroDNI) || numeroDNI < 0 || numeroDNI > 99999999) {
+                alert("Número de DNI no válido. Debe estar entre 0 y 99999999.");
+            } else {
+
+                const letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+                const resto = numeroDNI % 23;
+                const letra = letrasDNI.charAt(resto);
+
+                alert("La letra del DNI " + numeroDNI + " es: " + letra);
+            }
+        }
+
