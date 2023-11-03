1594b6605889a5cf6ad831a2df97caf9fd27d3f0
NicolasGrosso <grossonicolasorl@gmail.com>
2023-11-03 11:19:51 -0300

    Cree pagina base de html

---
+++ js/ejercicio10.js
@@ -0,0 +1,12 @@
+const filas = parseInt(prompt('Ingrese una cantidad de filas'));
+const columnas = parseInt(prompt('Ingrese una cantidad de columnas '));
+const totalCeldas = filas + columnas;
+document.write(`<table><tbody>`);
+for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
+document.write(` 
+    <tr>
+        <td>1</td>
+    </tr>
+`);}
+document.write(`</tbody></table>`);
+        
