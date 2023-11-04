b6d4d49e7110e521313e9ba595a718b0b4a29d27
NicolasGrosso <grossonicolasorl@gmail.com>
2023-11-04 16:05:14 -0300

    Termine hasta ejercicio numero 11

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,25 +1,26 @@
-const nombres = [];
-const edades = [];
+var nombres = [];
+var edades = [];
 
 
 for (var i = 0; i < 3; i++) {
-  const nombre = prompt("Introduce un nombre:");
-  const edad = parseInt(prompt("Introduce la edad de " + nombre + ":"));
+  var nombre = prompt("Introduce un nombre:");
+  var edad = parseInt(prompt("Introduce la edad de " + nombre + ":"));
 
   nombres.push(nombre);
   edades.push(edad);
 }
 
 
-const indiceMayor = 0;
+var indiceMayor = 0;
 for (var i = 1; i < edades.length; i++) {
   if (edades[i] > edades[indiceMayor]) {
     indiceMayor = i;
   }
 }
 
-const nombreMayor = nombres[indiceMayor];
-const edadMayor = edades[indiceMayor];
+var nombreMayor = nombres[indiceMayor];
+var edadMayor = edades[indiceMayor];
 
 
 alert("La persona mayor es " + nombreMayor + " con " + edadMayor + " años.");
+
