b658c9324eaf9014565c51156e21f8d9865c6c3a
NicolasGrosso <grossonicolasorl@gmail.com>
2023-11-04 16:04:32 -0300

    Ejercicio numero 11 probado y terminado

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,25 +1,25 @@
-var nombres = [];
-var edades = [];
+const nombres = [];
+const edades = [];
 
 
 for (var i = 0; i < 3; i++) {
-  var nombre = prompt("Introduce un nombre:");
-  var edad = parseInt(prompt("Introduce la edad de " + nombre + ":"));
+  const nombre = prompt("Introduce un nombre:");
+  const edad = parseInt(prompt("Introduce la edad de " + nombre + ":"));
 
   nombres.push(nombre);
   edades.push(edad);
 }
 
 
-var indiceMayor = 0;
+const indiceMayor = 0;
 for (var i = 1; i < edades.length; i++) {
   if (edades[i] > edades[indiceMayor]) {
     indiceMayor = i;
   }
 }
 
-var nombreMayor = nombres[indiceMayor];
-var edadMayor = edades[indiceMayor];
+const nombreMayor = nombres[indiceMayor];
+const edadMayor = edades[indiceMayor];
 
 
 alert("La persona mayor es " + nombreMayor + " con " + edadMayor + " años.");
