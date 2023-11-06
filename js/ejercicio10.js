cc047ff8ceab31d446a016cbf293abfe655e4b94
NicolasGrosso <grossonicolasorl@gmail.com>
2023-11-06 11:15:08 -0300

    Modifique codigo ejercicio 10 dado en clases

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -3,10 +3,13 @@ const columnas = parseInt(prompt('Ingrese una cantidad de columnas '));
 let totalCeldas = filas + columnas;
 document.write(`<table><tbody>`);
 for(let indiceFilas = 0; indiceFilas < filas; indiceFilas++){
-document.write(` 
-    <tr>
-        <td>1</td>
-    </tr>
-`);}
+document.write(`<tr>`);
+for(let indiceColumnas = 0; indiceColumnas < columnas; indiceColumnas++){
+    document.write(`<td>${totalCeldas}</td>`);
+    totalCeldas
+}
+ document.write(`</tr>`);
+}
 document.write(`</tbody></table>`);
+
         
